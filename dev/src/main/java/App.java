855643ec20ce6f9f96dc0cbed85df8e0a8051e76
APrudnik <aprudnik@gmail.com>
2023-07-11 16:34:03 +0300

    'first'

---
+++ dev/src/main/java/App.java
@@ -0,0 +1,8 @@
+import utils.SumCalculator;
+
+public class App {
+    public static void main(String[] args) {
+        SumCalculator sumCalculator = new SumCalculator();
+        System.out.println(sumCalculator.sum(3));
+    }
+}
